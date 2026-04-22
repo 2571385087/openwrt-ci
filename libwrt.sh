30513ec6ddc6de2b63dfbb593d66be6cb41fcaa8
2571385087 <39901460+2571385087@users.noreply.github.com>
2026-04-22 15:35:12 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,7 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/easytierMain
+cp -r package/easytierMain/luci-app-easytier package/luci-app-easytier
+cp -r package/easytierMain/easytier package/easytier
+rm -rf package/easytierMain
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
